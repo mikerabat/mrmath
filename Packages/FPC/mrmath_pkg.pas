b4cffc8be99294ce21acfeb4a707abe06de91582
mikerabat <m.rabatscher@gmail.com>
2019-07-02 10:50:36 +0200

    renamed FPC package files

--- Packages/FPC/mrmath_pkg.pas
+++ Packages/FPC/mrmath_pkg.pas
@@ -1,8 +1,22 @@
+// ###################################################################
+// #### This file is part of the mathematics library project, and is
+// #### offered under the licence agreement described on
+// #### http://www.mrsoft.org/
+// ####
+// #### Copyright:(c) 2011, Michael R. . All rights reserved.
+// ####
+// #### Unless required by applicable law or agreed to in writing, software
+// #### distributed under the License is distributed on an "AS IS" BASIS,
+// #### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// #### See the License for the specific language governing permissions and
+// #### limitations under the License.
+// ###################################################################
+
 { This file was automatically created by Lazarus. Do not edit!
   This source is only used to compile and install the package.
  }
 
-unit mrMath;
+unit mrMath_pkg;
 
 {$warn 5023 off : no warning about unused units}
 interface
@@ -60,5 +74,5 @@ begin
 end;
 
 initialization
-  RegisterPackage('mrMath', @Register);
+  RegisterPackage('mrMath_pkg', @Register);
 end.
